e370c3db9479218516855ea8f9bff9231ac02e16
Ricardo Perera <ricardoperera@Ricardos-MacBook-Pro.local>
2026-04-09 10:51:32 +0200

    fix/applying decoupled settings and removing sqls scripts

--- src/flink/jobs/run_finance_job.py
+++ src/flink/jobs/run_finance_job.py
@@ -3,26 +3,6 @@ from .flink_job import FlinkJob
 from pyflink.table import Table
 
 
-SINK_SCHEMA = """
-            CREATE TABLE btc_moving_avg (
-                window_start    TIMESTAMP(3),
-                window_end      TIMESTAMP(3),
-                ticker          STRING,
-                avg_price       DOUBLE,
-                ticks           BIGINT
-            ) """
-
-SINK_CONN_DETAILS = {
-    "connector": "jdbc",
-    "url": "jdbc:postgresql://postgres:5432/finance_db",
-    "table-name": "btc_moving_avg",
-    "username": "admin",
-    "password": "admin",
-    "sink.buffer-flush.max-rows": "10",
-    "sink.buffer-flush.interval": "5s",
-}
-
-
 class FinanceJob(FlinkJob):
     def _get_moving_avg(self, query: str) -> Table | None:
         if query:
@@ -30,21 +10,12 @@ class FinanceJob(FlinkJob):
             return moving_avg
         return None
 
-    def _sink_moving_avg_to_postgres(self, job_settings: JobSettings) -> None:
+    def _sink_moving_avg_to_postgres(self, insert_sql: str) -> None:
+        if not insert_sql:
+            self._logger.warning("No insert_sql provided to sink data, skipping ... ")
+            return
+
         self._logger.info("Sinking data to PostgreSQL")
-        insert_sql = f"""
-               INSERT INTO btc_moving_avg
-               SELECT
-                   window_start,
-                   window_end,
-                   ticker,
-                   AVG(price)  AS avg_price,
-                   COUNT(*)    AS ticks
-               FROM TABLE(
-                   TUMBLE(TABLE {job_settings.table_name}, DESCRIPTOR(proc_time), INTERVAL '5' SECONDS)
-               )
-               GROUP BY window_start, window_end, ticker
-           """
         statement_set = self.t_env.create_statement_set()
         statement_set.add_insert_sql(insert_sql)
         job_client = statement_set.execute().get_job_client()
@@ -63,8 +34,12 @@ class FinanceJob(FlinkJob):
                 self._print_table(table=result_table)
 
         else:
-            self._create_sink_table(schema=SINK_SCHEMA, conn_details=SINK_CONN_DETAILS)
-            self._sink_moving_avg_to_postgres(job_settings=job_settings)
+            sink_schema = job_settings.sqls.get("sink_price_moving_avg", {}).get("sink_schema")
+            sink_conn_details = job_settings.sqls.get("sink_price_moving_avg", {}).get("sink_conn_details")
+            sink_insert_sql = job_settings.sqls.get("sink_price_moving_avg", {}).get("insert_sql")
+
+            self._create_sink_table(schema=sink_schema, conn_details=sink_conn_details)
+            self._sink_moving_avg_to_postgres(insert_sql=sink_insert_sql)
 
     def start(self, job_settings) -> None:
         self._logger.info(f"Starting {self.job_name}")
