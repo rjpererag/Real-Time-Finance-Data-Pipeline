a839e35ba46b80ad5d2f9db7eadaf9134e9bae14
Ricardo Perera <ricardoperera@Ricardos-MacBook-Pro.local>
2026-04-10 15:00:33 +0200

    fix/validating and transoforming event_time and trade_time to valid timstamp str format. fix/putting data to queue after parsing in _handle_data

--- src/websockets/ws/binance.py
+++ src/websockets/ws/binance.py
@@ -1,11 +1,22 @@
+import asyncio
+
 from .ws import Websocket
 from ..dataclasses.binance import Stream
 import json
 
 
 class BinanceWebsocket(Websocket):
-    def __init__(self, symbol: str, stream: str = "aggTrade", base_url="wss://stream.binance.com:9443"):
+    def __init__(
+        self,
+        queue: asyncio.Queue,
+        symbol: str,
+        stream: str = "aggTrade",
+        base_url="wss://stream.binance.com:9443",
+        print_data: bool = False,
+    ):
+        self.queue = queue
         self.base_url = base_url
+        self.print = print_data
         url = self._get_url(symbol=symbol, stream=stream)
         super().__init__(url=url)
 
@@ -13,17 +24,20 @@ class BinanceWebsocket(Websocket):
         stream = f"{symbol.lower()}@{stream}"
         return f"{self.base_url}/ws/{stream}"
 
-    async def on_message(self, message) -> Stream | None:
+    async def on_message(self, message) -> None:
         self._logger.debug(f"Received message: {message}")
         data = json.loads(message)
-        parsed_data = await self._handle_data(data=data)
-        print(parsed_data)
+        await self._handle_data(data=data)
 
-    async def _handle_data(self, data: dict) -> Stream | None:
+    async def _handle_data(self, data: dict) -> None:
         if not data:
             return None
         try:
-            return self._parse(data=data)
+            parsed_data = self._parse(data=data)
+            if self.print:
+                print(parsed_data)
+            await self.queue.put(parsed_data)
+
         except Exception as e:
             self._logger.error(f"Error parsing: {str(e)}")
 
